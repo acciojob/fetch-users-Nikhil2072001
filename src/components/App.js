ceae23547ca4f440457145186a40a16442537c75
Nikhil77sutara <nikhilchoudhary@sutara.org>
2023-11-22 14:42:58 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
+import axios from "axios";
+import 'regenerator-runtime/runtime'
 
 const App = () => {
+  const [data,setData] = useState([]);
+  async function handleClick(){
+   const response =await axios.get("https://reqres.in/api/users");
+   setData(response.data.data);
+   console.log(response);
+  }
   return (
-    <div>
+    <div className="maindiv">
         {/* Do not remove the main div */}
+        <nav>
+          <h1>Blue Whales</h1>
+          <button className="btn" onClick={handleClick}> get User List</button>
+        </nav>
+        <table>
+          <thead className="tabhead">
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </thead>
+          <tbody>
+            {
+              data.map((data)=>{
+                return (
+                  <tr>
+                    <td>{data.first_name}</td>
+                    <td>{data.last_name}</td>
+                    <td>{data.email}</td>
+                    <td>
+                      <img src={data.avatar} alt="not available"/>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
     </div>
   )
 }
