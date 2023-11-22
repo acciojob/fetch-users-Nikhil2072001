f6234a7e7e86dd6e4e17ec3f690e38b25f36408a
Nikhil77sutara <nikhilchoudhary@sutara.org>
2023-11-22 15:27:48 +0530

    five

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,67 @@
-
-import React,{useState,useEffect} from "react";
+import React, { useEffect, useState } from 'react';
+import 'regenerator-runtime/runtime';
 import './../styles/App.css';
-import axios from "axios";
-import 'regenerator-runtime/runtime'
+import axios from 'axios';
 
 const App = () => {
-  const [data,setData] = useState([]);
-  async function handleClick(){
-   const response =await axios.get("https://reqres.in/api/users");
-   setData(response.data.data);
-   console.log(response);
-  }
+  const [people, setPeople] = useState([]);
+
+  const getUsers = async () => {
+    // const res = await fetch('https://reqres.in/api/users');
+    // const data = await res.json();
+    const data = await axios.get('https://reqres.in/api/users');
+    console.log(data.data.data);
+    setPeople(data.data.data);
+    console.log(process.env.YOUTUBE_KEY);
+  };
+
+  // useEffect(() => {
+  //   getUsers();
+  // }, []);
+
   return (
-    <div className="maindiv">
-        {/* Do not remove the main div */}
-        <nav>
-          <h1>Blue Whales</h1>
-          <button className="btn" onClick={handleClick}>Get User List</button>
-        </nav>
-        <table>
-          <thead className="tabhead">
-            <tr>
+    <div>
+      {/* Do not remove the main div */}
+      <header className="nav">
+        <h1>Blue Whales</h1>
+        <button className="btn" onClick={getUsers}>
+          Get User List
+        </button>
+      </header>
+      <table>
+        <thead>
+          <tr>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Email</th>
             <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.length === 0 ? (
+            <tr>
+              <td>
+                <b>No data found to display.</b>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            { data.length ===0 ? <tr><td><b>No data found to display.</b></td></tr> : 
-              data.map((data,indx)=>{
-                return (
-                  <tr key={indx}>
-                    <td>{data.first_name}</td>
-                    <td>{data.last_name}</td>
-                    <td>{data.email}</td>
-                    <td>
-                      <img src={data.avatar} alt="not available"/>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-           
-
-          </tbody>
-        </table>
+          ) : (
+            people.map((person, index) => {
+              return (
+                <tr key={index}>
+                  <td>{person.first_name}</td>
+                  <td>{person.last_name}</td>
+                  <td>{person.email}</td>
+                  <td>
+                    <img src={person.avatar} alt="" />
+                  </td>
+                </tr>
+              );
+            })
+          )}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
